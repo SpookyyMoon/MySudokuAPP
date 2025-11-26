d84ffdbbee616eee804a7d705f9de3a717755725
SpookyyMoon <spookyluna666@gmail.com>
2025-11-26 17:44:05 +0100

    Commit inicial, creación de vistas y clases básicas.

---
+++ app/src/main/java/com/example/mysudokuapp/Entidades/Puntuaciones.java
@@ -0,0 +1,47 @@
+package com.example.mysudokuapp.Entidades;
+
+public class Puntuaciones{
+    String nombreUsuario;
+    Number tiempoTardado;
+    int pistasUsadas;
+    String dificultad;
+
+    public Puntuaciones(String nombreUsuario, Number tiempoTardado, int pistasUsadas, String dificultad) {
+        this.nombreUsuario = nombreUsuario;
+        this.tiempoTardado = tiempoTardado;
+        this.pistasUsadas = pistasUsadas;
+        this.dificultad = dificultad;
+    }
+
+    public String getDificultad() {
+        return dificultad;
+    }
+
+    public void setDificultad(String dificultad) {
+        this.dificultad = dificultad;
+    }
+
+    public int getPistasUsadas() {
+        return pistasUsadas;
+    }
+
+    public void setPistasUsadas(int pistasUsadas) {
+        this.pistasUsadas = pistasUsadas;
+    }
+
+    public Number getTiempoTardado() {
+        return tiempoTardado;
+    }
+
+    public void setTiempoTardado(Number tiempoTardado) {
+        this.tiempoTardado = tiempoTardado;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+}
---
+++ app/src/main/java/com/example/mysudokuapp/Servicios/ApiService.java
@@ -0,0 +1,18 @@
+package com.example.mysudokuapp.Servicios;
+
+
+import com.example.mysudokuapp.Entidades.Puntuaciones;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface ApiService {
+
+}
